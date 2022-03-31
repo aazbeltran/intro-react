297469f2134c95b0679f7b0a0850ff4a79860f15
Alonso Alejandro Zuñiga Beltran <aazbeltran@outlook.com>
2022-03-31 14:12:56 -0600

    add a state variable for students

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ class App extends React.Component {
 
     this.state = {
       buttonClicked: "",
+      students: [],
       assignments: [] /*Below this line, add the students state variable*/,
       grades: {}
     };
