585b3e582241d66a9678afad92905d1d01e25e39
Alonso Alejandro Zuñiga Beltran <aazbeltran@outlook.com>
2022-03-31 14:15:09 -0600

    create addStudent method

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,9 @@ class App extends React.Component {
   }
 
   /*Write an addStudent method here*/
-  addStudent(student) {
+  addStudent(studentName) {
     this.setState({
-      students: this.state.students.concat(student)
+      students: this.state.students.concat(studentName)
     });
   }
 
